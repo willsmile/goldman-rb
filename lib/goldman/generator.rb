3d0dd9b2067318036962aa24cf5c72c7af44bfb0
Wei Chen <smile.v.chen@gmail.com>
2023-05-12 12:19:15 +0900

    Use the object of date range instead of date form

--- lib/goldman/generator.rb
+++ lib/goldman/generator.rb
@@ -4,16 +4,16 @@ require "date"
 
 module Goldman
   class Generator
-    def initialize(data, formatter, date_form)
+    def initialize(data, formatter, date_list)
       @data = data
       @formatter = formatter
-      @date_form = date_form
+      @date_list = date_list
     end
 
     def exec
       raise Goldman::OperationError.new("No data for schedule. Please define them in the config file.") if @data.empty?
 
-      @date_form.range.each do |date|
+      @date_list.each do |date|
         @data[date.cwday]&.each do |time|
           @formatter.schedule(date, time)
         end
--- test/test_generator.rb
+++ test/test_generator.rb
@@ -13,15 +13,14 @@ class TestGenerator < Minitest::Test
                       "2023/04/05(水) 12:00~13:00\n"\
                       "2023/04/05(水) 18:00~19:00\n"
     path = fixture_path("config.yml")
-    options = {
-      start_date: "2023-04-01",
-      end_date: "2023-04-05"
-    }
-    date_form = Goldman::DateForm.new(options)
+    date_list = Goldman::DateRange.new(start_date_str: "2023-04-01",
+                                       end_date_str: "2023-04-05",
+                                       day_str: nil,
+                                       week_str: nil).date_list
     config = Goldman::Config.new(path)
     formatter = Goldman::Formatter.new(config)
     data = Goldman::DataLoader.new(config).data
-    generator = Goldman::Generator.new(data, formatter, date_form)
+    generator = Goldman::Generator.new(data, formatter, date_list)
     output, _ = capture_io do
       generator.exec
     end
@@ -30,15 +29,14 @@ class TestGenerator < Minitest::Test
 
   def test_generator_without_data
     path = fixture_path("no_data_config.yml")
-    options = {
-      start_date: "2023-04-01",
-      end_date: "2023-04-05"
-    }
-    date_form = Goldman::DateForm.new(options)
+    date_list = Goldman::DateRange.new(start_date_str: "2023-04-01",
+                                       end_date_str: "2023-04-05",
+                                       day_str: nil,
+                                       week_str: nil).date_list
     config = Goldman::Config.new(path)
     formatter = Goldman::Formatter.new(config)
     data = Goldman::DataLoader.new(config).data
-    generator = Goldman::Generator.new(data, formatter, date_form)
+    generator = Goldman::Generator.new(data, formatter, date_list)
     assert_raises Goldman::OperationError do
       generator.exec
     end
