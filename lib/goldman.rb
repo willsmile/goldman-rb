20c57bfd1974d2109c0056ef0e601f21f620df2f
Wei Chen <smile.v.chen@gmail.com>
2023-05-15 18:20:54 +0900

    Move definition of const DEFAULT_FORMAT to Formatter

--- lib/goldman.rb
+++ lib/goldman.rb
@@ -9,20 +9,6 @@ require_relative "goldman/version"
 require_relative "goldman/wday"
 
 module Goldman
-  DEFAULT_FORMAT = {
-    schedule: "%{date}(%{wday}) %{time}",
-    date: "%Y/%m/%d",
-    wday: {
-      Monday: "Mon",
-      Tuesday: "Tue",
-      Wednesday: "Wed",
-      Thursday: "Thu",
-      Friday: "Fri",
-      Saturday: "Sat",
-      Sunday: "Sun"
-    }
-  }
-
   class Error < StandardError; end
   class ParameterError < Error; end
   class ConfigurationError < Error; end
