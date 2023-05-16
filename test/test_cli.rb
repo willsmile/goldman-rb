aaf6fe84c9e5bf3f4e4c0aee490b4e3f1d993057
Wei Chen <smile.v.chen@gmail.com>
2023-05-16 14:27:00 +0900

    Add tests for Cli and create a method to slient output in test helper

--- test/test_cli.rb
+++ test/test_cli.rb
@@ -5,21 +5,36 @@ require "test_helper"
 class TestCli < Minitest::Test
   def test_cli_with_default_argv
     argv = ["-g"]
-    Goldman::Cli.new(argv).run
+    slient_output { Goldman::Cli.new(argv).run }
   end
 
-  def test_cli_with_often_used_argv
-    argv = ["-g", "-s", "2023-05-01", "-d", "10"]
-    Goldman::Cli.new(argv).run
+  def test_cli_with_argv_of_start_date_and_end_date
+    argv = ["-g", "-s", "2023-05-01", "-e", "2023-05-10"]
+    slient_output { Goldman::Cli.new(argv).run }
+  end
+
+  def test_cli_with_argv_of_start_date_and_day
+    argv = ["-g", "-s", "2023-05-01", "-d", "3"]
+    slient_output { Goldman::Cli.new(argv).run }
+  end
+
+  def test_cli_with_argv_of_start_date_and_week
+    argv = ["-g", "-s", "2023-05-01", "-w", "1"]
+    slient_output { Goldman::Cli.new(argv).run }
   end
 
   def test_cli_with_invalid_argv
     argv = ["-x"]
-    Goldman::Cli.new(argv).run
+    assert_raises OptionParser::InvalidOption do
+      slient_output { Goldman::Cli.new(argv).run }
+    end
   end
 
   def test_cli_with_none_of_argv
     argv = []
-    Goldman::Cli.new(argv).run
+    expected_output = ""
+    assert_output(expected_output) do
+      Goldman::Cli.new(argv).run
+    end
   end
 end
